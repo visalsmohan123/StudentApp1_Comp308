c501e7e82bbd0e262673131134807435e7ddf68c
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-04-03 15:30:38 -0400

    Updated build file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/StudentForm.jsx',
+      name: 'student-form-library',
+      formats: ['es', 'cjs'],
+    },
+  },
+});
