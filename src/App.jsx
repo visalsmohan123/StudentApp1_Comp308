e3b0c7be3dbbfa49b0af002a01db90814e68725b
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-04-03 02:04:15 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header.jsx";
+import StudentForm from "./StudentForm.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <StudentForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/StudentForm.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+
+function StudentForm() {
+  return (
+    <>
+      <div class="container ">
+        <form action="">
+        <input type="text" placeholder="Student Id" name="id" required />
+
+          <input type="text" placeholder="Name" name="name" required />
+          <input type="email" placeholder="Email" name="email" required />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            name="phoneNumber"
+            required
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default StudentForm;
