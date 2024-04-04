6fbe8af1964f70d6c1c75fd6532bae55d405eb05
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-04-03 22:38:22 -0400

    updated form

--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -6,6 +6,7 @@ function StudentForm() {
     <>
       <div class="container ">
         <form action="">
+        <input type="text" placeholder="Student Id" name="id" required />
 
           <input type="text" placeholder="Name" name="name" required />
           <input type="email" placeholder="Email" name="email" required />
